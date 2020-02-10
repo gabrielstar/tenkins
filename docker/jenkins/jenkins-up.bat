f7e78a14ebfef46bb831a0227133e9b449d98681
gabrielstar <gabriel.starczewski@gmail.com>
2020-02-10 08:05:51 +0100

    adding bat script

---
+++ docker/jenkins/jenkins-up.bat
@@ -0,0 +1,6 @@
+::this is reduced script due to many issues with docker on windows 10, fall back to manual as last resort
+SET image_name=tenkins
+SET loacal_port=9090
+docker container stop %image_name%
+docker build --no-cache -t %image_name% .
+docker run -p %local_port%:8080 -v downloads:/var/jenkins_home/downloads -v jobs:/var/jenkins_home/jobs/ --rm --name %image_name% %image_name%:latest
--- docker/jenkins/jenkins-up.sh
+++ docker/jenkins/jenkins-up.sh
@@ -23,8 +23,8 @@ echo "Stopping existing container if running"
 docker container stop $jenkins_container_name > /dev/null 2>&1 || :
 #DOWNLOAD TOOLS, NEED TO BE DONE ONCE ONLY
 
-#source downloads/download_tools.sh $maven_file $maven_url $jdk8_file $jdk8_url
-source downloads/download_tools.sh $maven_file $maven_url $jdk8_file $jdk8_url remove_existing_binaries
+source downloads/download_tools.sh $maven_file $maven_url $jdk8_file $jdk8_url
+#source downloads/download_tools.sh $maven_file $maven_url $jdk8_file $jdk8_url remove_existing_binaries
 
 cd $current_path
 
@@ -33,7 +33,9 @@ echo "Building image"
 docker build --no-cache -t "$jenkins_container_name" .
 # run container so it is removed after stopping, from https://hub.docker.com/r/jenkins/jenkins, preserve jobs
 echo "Running jenkins image on http://"`docker-machine ip default`":$local_port"
-docker run -p $local_port:8080 -v `pwd`/downloads:/var/jenkins_home/downloads -v `pwd`/jobs:/var/jenkins_home/jobs/ --rm --name "$jenkins_container_name" "$jenkins_container_name":latest
+#docker run -p $local_port:8080 -v `pwd`/downloads:/var/jenkins_home/downloads -v `pwd`/jobs:/var/jenkins_home/jobs/ --rm --name "$jenkins_container_name" "$jenkins_container_name":latest
+#docker run -p 9090:8080 -v downloads:/var/jenkins_home/downloads  --rm --name tenkins tenkins:latest
+docker run -p $local_port:8080 -v `pwd`/downloads:/var/jenkins_home/downloads  --rm --name "$jenkins_container_name" "$jenkins_container_name":latest
 
 #process stops here, now execute manually to test all gogg
 docker exec -it tenkins ls -l /var/jenkins_home/
